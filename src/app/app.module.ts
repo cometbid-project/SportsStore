1056de48bfd76413155d775d18422beb19f4028d
Gbenga <gbenga_java@yahoo.com>
2019-11-05 21:02:08 -0400

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StoreModule } from "./store/store.module";
+import { StoreComponent } from "./store/store.component";
+import { CheckoutComponent } from "./store/checkout.component";
+import { CartDetailComponent } from "./store/cartDetail.component";
+import { RouterModule } from "@angular/router";
+import { StoreFirstGuard } from './storeFirst.guard';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    StoreModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
+  providers: [StoreFirstGuard],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
